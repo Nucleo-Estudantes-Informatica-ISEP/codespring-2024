6c3d28ac482d4f7864f39dc1f865736a7eeebcab
Tomás Lopes <1211289@isep.ipp.pt>
2024-02-18 17:47:46 +0000

    wip: started faq section

---
+++ src/components/faq/Question.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Question: React.FC = () => {
+  return (
+    <section>
+      <h2>Question</h2>
+      <p>Answer</p>
+    </section>
+  );
+};
+
+export default Question;
