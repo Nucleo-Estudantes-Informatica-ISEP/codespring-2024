2741a63ffb74a66476fb4de7d6bc2bfec3c9d729
André L Reis <gato5andre26@gmail.com>
2024-03-02 23:29:55 +0000

    enhancement: component's colors moved to global styles + changes to the colors

--- src/components/schedule/ScheduleContainer.tsx
+++ src/components/schedule/ScheduleContainer.tsx
@@ -2,11 +2,11 @@ import schedule from "../../data/schedule.json";
 
 const ScheduleContainer: React.FC = () => {
     return (
-        <div className="w-full rounded-lg dark:bg-[#3b3b3b] bg-secondary flex">
+        <div className="w-full rounded-lg bg-sectionCard flex flex-col md:flex-row md:flex-wrap">
             {schedule.map((day, index) => (
-                <div key={index} className={`flex-1 pl-8 py-8 ${index !== schedule.length - 1 ? 'border-r border-white' : ''}`}>
-                    <div className="flex items-center flex-1 pb-4">
-                        <div className="bg-neutral-400 dark:bg-[#000614] p-1 flex">
+                <div key={index} className={`flex-1 p-8 ${index !== schedule.length - 1 ? 'border-b border-white md:border-b-0 md:border-r md:border-white' : ''}`}>
+                    <div className="flex items-center pb-4">
+                        <div className="bg-scheduleDay p-1 flex">
                             <p className="text-white text-xl font-light text-left px-1">{day.day}</p>
                         </div>
                         <p className="text-xl font-light text-left pl-4">{day.hour ? day.hour + " - " + day.location : day.location}</p>
