05775a5b894c6cd968ec7176f726006ac15b0f5d
André L Reis <gato5andre26@gmail.com>
2024-03-23 14:07:53 +0000

    fix: card borders

--- src/components/juriCriteria/JuriCriteriaContainer.tsx
+++ src/components/juriCriteria/JuriCriteriaContainer.tsx
@@ -6,7 +6,7 @@ const JuriCriteriaContainer: React.FC = () => {
       <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
         {juriCriteria.map((criteria, index) => (
           <div key={index} className="w-72 h-96 mx-auto">
-            <div className="h-full flex flex-col justify-between bg-background rounded-xl overflow-hidden shadow-lg dark:shadow-none border border-none dark:border-solid">
+            <div className="h-full flex flex-col justify-between bg-background rounded-xl overflow-hidden shadow-xl dark:shadow-none border border-b-0 dark:border-b">
               <div className="flex justify-center pt-12">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="w-32 h-32 mx-auto">
                   <g fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2">
@@ -19,7 +19,7 @@ const JuriCriteriaContainer: React.FC = () => {
               <div className="flex justify-center px-4 py-2">
                 <p className="mt-2 font-semibold text-lg text-center">{criteria.description}</p>
               </div>
-              <div className="bg-gradient-to-r from-gray-300 to-gray-500 px-4 py-3">
+              <div className="bg-gradient-to-r from-red-300 to-red-800 px-4 py-3">
                 <p className="text-white text-center">{criteria.value}%</p>
               </div>
             </div>
