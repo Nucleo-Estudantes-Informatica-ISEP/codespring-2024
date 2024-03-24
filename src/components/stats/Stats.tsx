83b85f3273de3f7a9b655ed7d7494eb10f5a864d
André Barros <1211299@isep.ipp.pt>
2024-03-24 22:36:40 +0000

    fix: overall spacing and font size

--- src/components/stats/Stats.tsx
+++ src/components/stats/Stats.tsx
@@ -5,16 +5,16 @@ import stats from "../../data/stats.json";
 const StatsContainer: React.FC = () => {
   return (
     <div className="flex w-full justify-center">
-      <div className="grid w-full max-w-screen-lg grid-cols-1 justify-center gap-6 font-sans sm:grid-cols-2 lg:grid-cols-4">
+      <div className="grid w-full max-w-screen-lg grid-cols-1 justify-center gap-x-64 gap-y-12 sm:grid-cols-2 lg:grid-cols-4">
         {stats.map((stat, index) => (
           <div
             key={index}
             className="flex flex-col items-center justify-center text-center"
           >
-            <span className="text-5xl font-extrabold text-accent md:text-6xl lg:text-7xl">
+            <span className="text-6xl font-bold text-accent md:text-7xl lg:text-8xl">
               {stat.title}
             </span>
-            <span className="mt-2 text-lg font-bold md:text-xl lg:text-2xl">
+            <span className="mt-2 text-xl md:text-2xl lg:text-3xl">
               {stat.description}
             </span>
           </div>
