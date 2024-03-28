5f8b420b14682327e75ae1dfcb26420dbe9a51dd
Tomás Lopes <1211289@isep.ipp.pt>
2024-03-28 11:24:48 +0000

    feat: added rules

--- src/components/rules/RulesContainer.tsx
+++ src/components/rules/RulesContainer.tsx
@@ -1,17 +1,35 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 import rules from "../../data/rules.json";
 
 const RulesContainer: React.FC = () => {
   return (
-    <div className="mx-auto w-full items-center justify-center md:w-1/2 md:px-8">
+    <div className="mx-auto w-full items-center justify-center bg-background md:w-1/2 md:px-8">
       {rules.map((rule, index) => (
-        <div key={index} className="mb-3">
+        <motion.div
+          initial={{
+            opacity: 0,
+            x: -100
+          }}
+          animate={{
+            opacity: 1,
+            x: 0
+          }}
+          transition={{
+            duration: 0.2 * (index + 1)
+          }}
+          viewport={{
+            once: true
+          }}
+          key={rule.title}
+          className="mb-4"
+        >
           <h3 className="mb-2 text-xl font-bold">
-            {index + 1}. {rule.rule}
+            {index + 1}. {rule.title}
           </h3>
           <p className="">{rule.text}</p>
-        </div>
+        </motion.div>
       ))}
     </div>
   );
