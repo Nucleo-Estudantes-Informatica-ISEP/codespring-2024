c2b8b0942d36a35dbf0510c414581bac9c0bc5d7
André Barros <1211299@isep.ipp.pt>
2024-04-01 22:26:47 +0100

    chore: added sponsors images

--- src/components/sponsors/SponsorCard.tsx
+++ src/components/sponsors/SponsorCard.tsx
@@ -1,32 +1,27 @@
-import type { Sponsor } from "@/components/sponsors/SponsorsInfo.ts";
+import { useState } from "react";
+
 import { Card, CardContent } from "@/lib/components/ui/card.tsx";
 import { CarouselItem } from "@/lib/components/ui/carousel.tsx";
-import { useState } from "react";
+import type { Sponsor } from "@/components/sponsors/SponsorsInfo.ts";
 
 interface SponsorCardProps {
   sponsor: Sponsor;
 }
 
 function SponsorCard(props: SponsorCardProps) {
-
-  const sponsor : Sponsor = props.sponsor;
-  const [theme, setTheme] = useState<string>('dark');
+  const sponsor: Sponsor = props.sponsor;
+  const [theme, setTheme] = useState<string>("dark");
   // for now, default theme, but TO-DO: study how to get the theme from the localStorage
 
   return (
-    <CarouselItem
-      className='basis-1/2 md:basis-1/3 flex items-center select-none'
-    >
-      <Card className='transition-all duration-500 rounded-lg md:hover:rounded-3xl'>
-        <a
-          target='_blank'
-          href={sponsor.href || ''}
-        >
-          <CardContent className='flex aspect-square items-center justify-center p-6'>
+    <CarouselItem className="flex basis-1/2 select-none items-center md:basis-1/3">
+      <Card className="rounded-lg transition-all duration-500 md:hover:rounded-3xl">
+        <a target="_blank" href={sponsor.href || ""}>
+          <CardContent className="flex aspect-square items-center justify-center p-6">
             <img
-              src={theme == 'dark' ? sponsor.img : sponsor.imgLight || sponsor.img}
+              src={theme == "dark" ? sponsor.img : sponsor.imgLight}
               alt={sponsor.alt}
-              className={`${sponsor.style || ''}`}
+              className={`${sponsor.style || ""}`}
             />
           </CardContent>
         </a>
--- src/components/sponsors/SponsorsInfo.ts
+++ src/components/sponsors/SponsorsInfo.ts
@@ -1,15 +1,10 @@
 export default class SponsorsInfo {
-  public static getAllSponsors() : Sponsor[] {
+  public static getAllSponsors(): Sponsor[] {
     return [
-        {img: "/logo/DELOITTE_DARK.jpg", imgLight: "/logo/DELOITTE_LIGHT.jpg", alt: "NO IMAGE"},
-        {img: "/logo/DELOITTE_DARK.jpg", imgLight: "/logo/DELOITTE_LIGHT.jpg", alt: "NO IMAGE"},
-        {img: "/logo/DELOITTE_DARK.jpg", imgLight: "/logo/DELOITTE_LIGHT.jpg", alt: "NO IMAGE"},
-        {img: "/logo.png", alt: "NO IMAGE"},
-        {img: "/logo.png", alt: "NO IMAGE"},
-        {img: "/logo.png", alt: "NO IMAGE"},
-        {img: "/nei-logo.webp", alt: "NO IMAGE", style: "invert dark:invert-0"},
-        {img: "/nei-logo.webp", alt: "NO IMAGE", style: "invert dark:invert-0"},
-        {img: "/nei-logo.webp", alt: "NO IMAGE", style: "invert dark:invert-0"},
+      { img: "/logo/deloitte-white.png", imgLight: "/logo/deloitte.webp", alt: "Deloitte Logo", href: "https://www2.deloitte.com/pt/pt.html" },
+      { img: "/logo/critical_software_main_logo.png", alt: "Critical Software Logo", href: "https://www.criticalsoftware.com/pt" },
+      { img: "/nei-logo.webp", alt: "NEI Logo", style: "invert dark:invert-0", href: "https://www.nei-isep.org/" },
+      { img: "/logo/redBull.png", alt: "RedBull Logo", href: "https://www.redbull.com/pt-pt/" },
     ];
   }
 }
