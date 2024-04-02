b0ea30a4c0dcad38b0aeb79ab4db2904ac3d79a2
André Barros <1211299@isep.ipp.pt>
2024-04-02 23:29:15 +0100

    fix: awards animation and gap

--- src/components/awards/AwardsContainer.tsx
+++ src/components/awards/AwardsContainer.tsx
@@ -6,11 +6,11 @@ import { BackgroundGradient } from "../../lib/components/ui/background-gradient"
 
 export default function AwardsContainer() {
   return (
-    <section className="grid grid-cols-1 place-items-center gap-y-4 md:grid-cols-3 md:gap-2">
+    <section className="grid grid-cols-1 place-items-center gap-y-4 md:grid-cols-3 md:gap-x-2 md:gap-y-0">
       <BackgroundGradient className="flex h-full w-full max-w-xs flex-col justify-center rounded-[22px] bg-background p-4 text-center dark:bg-zinc-900 sm:p-5 md:h-48">
         <motion.p
           initial={{ opacity: 0.1, y: 0 }}
-          whileInView={{ opacity: 1, y: 0 }}
+          animate={{ opacity: 1, y: 0 }}
           transition={{
             delay: 0.3,
             duration: 0.8,
@@ -24,7 +24,7 @@ export default function AwardsContainer() {
       <BackgroundGradient className="flex h-full w-full max-w-xs flex-col justify-center rounded-[22px] bg-background p-4 text-center dark:bg-zinc-900 sm:p-5 md:h-48">
         <motion.p
           initial={{ opacity: 0.1, y: 0 }}
-          whileInView={{ opacity: 1, y: 0 }}
+          animate={{ opacity: 1, y: 0 }}
           transition={{
             delay: 0.3,
             duration: 0.8,
@@ -38,7 +38,7 @@ export default function AwardsContainer() {
       <BackgroundGradient className="flex h-full w-full max-w-xs flex-col justify-center rounded-[22px] bg-background p-4 text-center dark:bg-zinc-900 sm:p-5 md:h-48">
         <motion.p
           initial={{ opacity: 0.1, y: 0 }}
-          whileInView={{ opacity: 1, y: 0 }}
+          animate={{ opacity: 1, y: 0 }}
           transition={{
             delay: 0.3,
             duration: 0.8,
