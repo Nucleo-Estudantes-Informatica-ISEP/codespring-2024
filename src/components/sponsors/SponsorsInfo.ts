eb8e11f52cee38b64e3cb1265f899e4c24585a9c
henriquelspinto <1211201@isep.ipp.pt>
2024-04-03 11:42:07 +0100

    feat: final sponsors

--- src/components/sponsors/SponsorsInfo.ts
+++ src/components/sponsors/SponsorsInfo.ts
@@ -2,8 +2,8 @@ export default class SponsorsInfo {
   public static getAllSponsors(): Sponsor[] {
     return [
       { img: "/logo/deloitte-white.png", imgLight: "/logo/deloitte.webp", alt: "Deloitte Logo", href: "https://www2.deloitte.com/pt/pt.html" },
-      { img: "/logo/critical_software_main_logo.png", alt: "Critical Software Logo", href: "https://www.criticalsoftware.com/pt" },
-      { img: "/logo/redBull.png", alt: "RedBull Logo", href: "https://www.redbull.com/pt-pt/" },
+      { img: "/logo/LogoCriticalSoftware.png", alt: "Critical Software Logo", href: "https://www.criticalsoftware.com/pt" },
+      { img: "/logo/blackroute.jpg", alt: "Blackroute Logo", href: "", style: "rounded-3xl" },
     ];
   }
 }
