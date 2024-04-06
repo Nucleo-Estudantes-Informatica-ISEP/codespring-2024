6914dfa4b08d0c91e440018bf44dccd2fa3dcb28
André Barros <1211299@isep.ipp.pt>
2024-04-06 20:36:14 +0100

    chore: added juri photos & changed style

--- src/components/juri/JuriContainer.tsx
+++ src/components/juri/JuriContainer.tsx
@@ -5,16 +5,17 @@ import { AnimatedTooltip } from "@/lib/components/ui/animated-tooltip";
 const people = [
   {
     id: 1,
-    name: "Tomás Lopes AKA Malo",
-    designation: "Soon to be Software Engineer ou Lixeiro",
-    image: "/foto.jpg"
+    name: "Paulo Gandra de Sousa",
+    designation:
+      "Professor no Departamento de Engenharia Informática do ISEP desde 1996, sendo atualmente Professor Coordenador. Doutorado em Informática pela Universidade do Minho, a sua área de especialização foi em sistemas distribuídos inteligentes para a manufatura. Desde 2012 é Consultor em Engenharia de Software numa empresa multinacional, msg Life Iberia",
+    image: "/paulo-gandra.jpeg"
   },
   {
     id: 2,
-    name: "Robert Johnson",
-    designation: "Product Manager",
-    image:
-      "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60"
+    name: "Tomás Lima",
+    designation:
+      "Tomás tem mais de uma década de experiência no setor de segurança cibernética, com especialização em inteligência e automação de ameaças cibernéticas. O seu percurso profissional inclui uma passagem significativa no Centro Nacional de Cibersegurança de Portugal, onde desempenhou um papel no desenvolvimento da ferramenta IntelMQ. Atualmente, Tomás dedica a sua experiência à oferta de serviços de consultoria em cibersegurança na Europa e à liderança da sua nova empresa, a BlackRoute.",
+    image: "/tomas-lima.jpeg"
   },
   {
     id: 3,
@@ -29,26 +30,12 @@ const people = [
     designation: "UX Designer",
     image:
       "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGF2YXRhcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
-  },
-  {
-    id: 5,
-    name: "Tyler Durden",
-    designation: "Soap Developer",
-    image:
-      "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3540&q=80"
-  },
-  {
-    id: 6,
-    name: "Dora",
-    designation: "The Explorer",
-    image:
-      "https://images.unsplash.com/photo-1544725176-7c40e5a71c5e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3534&q=80"
   }
 ];
 
 export default function JuriContainer() {
   return (
-    <div className="mb-10 flex w-full flex-row items-center justify-center">
+    <div className="mb-10 flex w-full items-center justify-center">
       <AnimatedTooltip items={people} />
     </div>
   );
--- src/lib/components/ui/animated-tooltip.tsx
+++ src/lib/components/ui/animated-tooltip.tsx
@@ -24,7 +24,7 @@ export const AnimatedTooltip = ({
   const x = useMotionValue(0); // going to set this value on mouse move
   // rotate the tooltip
   const rotate = useSpring(
-    useTransform(x, [-100, 100], [-45, 45]),
+    useTransform(x, [-100, 100], [-30, 30]),
     springConfig
   );
   // translate the tooltip
@@ -38,10 +38,10 @@ export const AnimatedTooltip = ({
   };
 
   return (
-    <div className="grid grid-cols-2 gap-x-3 gap-y-2 md:grid-cols-2 md:gap-x-0 md:gap-y-4 lg:grid-cols-3">
+    <div className="grid grid-cols-2 gap-x-3 gap-y-2 md:flex md:grid-cols-none md:gap-x-0 md:gap-y-4">
       {items.map((item, idx) => (
         <div
-          className="group relative -mr-4"
+          className="group relative -mr-4 w-full"
           key={item.name}
           onMouseEnter={() => setHoveredIndex(item.id)}
           onMouseLeave={() => setHoveredIndex(null)}
@@ -62,17 +62,18 @@ export const AnimatedTooltip = ({
               exit={{ opacity: 0, y: 20, scale: 0.6 }}
               style={{
                 translateX: translateX,
-                rotate: rotate,
-                whiteSpace: "nowrap"
+                rotate: rotate
               }}
-              className="absolute -top-16 z-50 flex translate-x-1/2 flex-col items-center justify-center rounded-md bg-black px-4 py-2 text-xs shadow-xl"
+              className="absolute -bottom-32 z-50 flex flex-col items-center justify-center rounded-md bg-black px-4 py-2 shadow-xl"
             >
               <div className="absolute inset-x-10 -bottom-px z-30 h-px w-[20%] bg-gradient-to-r from-transparent via-emerald-500 to-transparent " />
               <div className="absolute -bottom-px left-10 z-30 h-px w-[40%] bg-gradient-to-r from-transparent via-sky-500 to-transparent " />
-              <div className="relative z-30 text-base font-bold text-white">
+              <div className="relative z-30 text-xl font-bold text-white">
                 {item.name}
               </div>
-              <div className="text-xs text-white">{item.designation}</div>
+              <div className="text-xs text-white" style={{ width: "300px" }}>
+                {item.designation}
+              </div>
             </motion.div>
           )}
           <img
