8e6ef9c0079d489bdbcfe74aff6f62c8260df0d4
André Barros <1211299@isep.ipp.pt>
2024-04-16 09:26:07 +0100

    chore: added sponsors part II

---
+++ src/components/sponsorsII/SponsorsContainerII.tsx
@@ -0,0 +1,35 @@
+import {
+  Carousel,
+  CarouselContent,
+  CarouselNext,
+  CarouselPrevious
+} from "@/lib/components/ui/carousel.tsx";
+import SponsorCard from "@/components/sponsorsII/SponsorsCardII";
+import {
+  getAllInfo,
+  type Sponsor
+} from "@/components/sponsorsII/SponsorsInfoII";
+
+function ThanksContainer() {
+  const sponsors: Sponsor[] = getAllInfo();
+
+  return (
+    <div className="flex w-full justify-center">
+      <Carousel className="w-full max-w-80 md:max-w-3xl lg:max-w-6xl">
+        <CarouselContent className="-ml-1">
+          {sponsors.map((sponsor, index) => (
+            <SponsorCard sponsor={sponsor} key={index} />
+          ))}
+        </CarouselContent>
+        <CarouselPrevious
+          className={sponsors.length < 3 ? "hidden md:block" : ""}
+        />
+        <CarouselNext
+          className={sponsors.length < 3 ? "hidden md:block" : ""}
+        />
+      </Carousel>
+    </div>
+  );
+}
+
+export default ThanksContainer;
---
+++ src/components/sponsorsII/SponsorsInfoII.ts
@@ -0,0 +1,47 @@
+export const getAllInfo = (): Sponsor[] => {
+  return [
+    {
+      img: "/logo/aeisep.png",
+      alt: "AEIsep Logo",
+      href: "https://www.aeisep.pt/"
+    },
+    {
+      img: "/logo/dei.png",
+      alt: "DEI Logo",
+      href: "https://www.dei.isep.ipp.pt/"
+    },
+    {
+      img: "/logo/redBull.png",
+      alt: "RedBull Logo",
+      href: "https://www.redbull.com/"
+    },
+    {
+      img: "/logo/dominos.png",
+      alt: "Dominos Logo",
+      href: "https://www.dominos.pt/"
+    },
+    {
+      img: "/logo/vitalis.png",
+      alt: "Vitalis Logo",
+      href: "https://www.vitalis.pt/"
+    },
+    {
+      img: "/logo/divinal.png",
+      alt: "Divinal Logo",
+      href: "https://www.divinal.pt/"
+    },
+    {
+      img: "/logo/lusoImpress.png",
+      alt: "LusoImpress Logo",
+      href: "https://lusoimpress.com/"
+    }
+  ];
+};
+
+export interface Sponsor {
+  img: string;
+  imgLight?: string;
+  alt: string;
+  style?: string;
+  href?: string;
+}
